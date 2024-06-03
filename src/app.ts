fb7d5d514738e0a53f2293e62ab21d7527109e6b
marcebenitez2 <marcebenitez0607@gmail.com>
2024-06-03 16:45:19 -0300

    feat: Envio de mensajes y archivos pdf

--- src/app.ts
+++ src/app.ts
@@ -3,55 +3,96 @@ import { MemoryDB as Database } from "@builderbot/bot";
 import { BaileysProvider as Provider } from "@builderbot/provider-baileys";
 import { allFlows } from "./allflows";
 import cors from "cors";
+import multer from "multer";
+import path, { join } from "path";
+import fs from "fs";
 
-const PORT = process.env.PORT ?? 3008
+const PORT = process.env.PORT ?? 3008;
 
 const main = async () => {
-    const adapterProvider = createProvider(Provider);
-    const adapterDB = new Database();
-    const adapterFlow = createFlow(allFlows);
+  const adapterProvider = createProvider(Provider);
+  const adapterDB = new Database();
+  const adapterFlow = createFlow(allFlows);
 
-    const { handleCtx, httpServer } = await createBot({
-        flow: adapterFlow,
-        provider: adapterProvider,
-        database: adapterDB,
-    });
+  const { handleCtx, httpServer } = await createBot({
+    flow: adapterFlow,
+    provider: adapterProvider,
+    database: adapterDB,
+  });
 
-    adapterProvider.server.use(cors({ origin: "*", methods: ["GET", "POST"] }));
-    adapterProvider.server.get("/funciona", (req, res) => res.end("Funciona"));
+  const storage = multer.diskStorage({
+    destination: (req, file, cb) => {
+      cb(null, "uploads/");
+    },
+    filename: (req, file, cb) => {
+      cb(null, file.originalname);
+    },
+  });
 
-    adapterProvider.server.post(
-        "/enviar-mensajes",
-        handleCtx(async (bot, req, res) => {
-            const { phones, ganador } = req.body;
-            console.log(phones);
-            console.log(ganador);
+  const upload = multer({ storage });
 
-            if (!Array.isArray(phones) || phones.length === 0) {
-                return res.status(400).send("Formato de datos incorrecto o sin contactos.");
+  adapterProvider.server.use(cors({ origin: "*", methods: ["GET", "POST"] }));
+  adapterProvider.server.get("/funciona", (req, res) => res.end("Funciona"));
+
+  adapterProvider.server.post(
+    "/enviar-mensajes",
+    upload.single("file"),
+    handleCtx(async (bot, req, res) => {
+      try {
+        const { phones: phonesString, message: messageString } = req.body;
+        const file = req.file;
+
+        if (!phonesString) {
+          res.end("Faltaron los numeros de telefono");
+          return;
+        }
+
+        let phones;
+        try {
+          phones = JSON.parse(phonesString);
+        } catch (error) {
+          console.error("Error al parsear 'phones':", error);
+          res.end("Formato de 'phones' inválido");
+          return;
+        }
+
+        if (!Array.isArray(phones)) {
+          res.end("'phones' debe ser un array");
+          return;
+        }
+
+        const promises = phones.map(async (phone) => {
+          const numero = `549${phone.tel}`;
+          const message = messageString;
+
+          if (file) {
+            const pathLocal = join("uploads/", file.originalname);
+
+            if (message !== "") {
+              // En caso que haya mensaje se envia el mensaje y el archivo
+              await bot.sendMessage(numero, message, {});
+              await bot.sendMessage(numero, message, { media: pathLocal });
+            } else {
+              //  En caso que no haya mensaje se envia solo el archivo
+              await bot.sendMessage(numero, message, { media: pathLocal });
             }
 
-            try {
-                const promises = phones.map(async (phone) => {
+            // En caso que no haya archivo se envia solo el mensaje
+          } else {
+            await bot.sendMessage(numero, message, {});
+          }
+        });
 
-                    const message = `Hola ${phone.apellido}, te escribimos de la mutual de Gendarmería Nacional para informarte que el ganador del sorteo mensual es el cliente número ${ganador[0].cliente}, con el premio ${ganador[0].pre_pen}.`;
-                    const numero = `549${phone.tel}`;
+        await Promise.all(promises);
+        res.end("Mensajes enviados");
+      } catch (error) {
+        console.error("Error en el manejo del mensaje:", error);
+        res.end("Error interno del servidor");
+      }
+    })
+  );
 
-                    await bot.sendMessage(numero, message, {});
-
-                });
-
-                await Promise.all(promises);
-                res.end("Mensajes enviados.");
-
-            } catch (error) {
-                console.error("Error al enviar mensajes:", error);
-                res.status(500).send("Error al enviar mensajes");
-            }
-        })
-    );
-
-    httpServer(+PORT);
+  httpServer(+PORT);
 };
 
-main()
+main();
